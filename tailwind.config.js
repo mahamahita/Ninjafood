241f0852a9a311566d384a1cc0c59a6fbbae45e1
Maha EL OUARDI <98224920+mahamahita@users.noreply.github.com>
2024-07-12 21:15:04 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [   
+    './dist/**/*.html', 
+    './src/**/*.html', 
+    './src/**/*.js',  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
